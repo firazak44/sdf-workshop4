bb3ed0d0a28977357e8463b7cf53316dfeba925d
Firdaus Razak <razakfirdaus95@gmail.com>
2022-06-19 23:09:12 +0800

    apptestNG

--- sdf-workshop4/src/test/java/vtpday4workshop/AppTest.java
+++ sdf-workshop4/src/test/java/vtpday4workshop/AppTest.java
@@ -1,8 +1,11 @@
 package vtpday4workshop;
 
-import static org.junit.Assert.assertTrue;
+import vtpday4workshop.server.fortunecookie;
 
-import org.junit.Test;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 /**
  * Unit test for simple App.
@@ -13,8 +16,18 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+    public void testCookie() {
+        String randomCookie = fortunecookie
+          .getRandomCookie("/Users/skyho/OneDrive/Documents/VISAJava/Sdf-Workshop4/sdf-workshop4/cookie_file.txt");
+        Assert.assertNull(randomCookie);
+      }
+      
+      @BeforeTest
+      public void beforeTest() {
+      }
+    
+      @AfterTest
+      public void afterTest() {
+    
+      }
 }
